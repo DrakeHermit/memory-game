f3512bd2607e1c577442c3fae539161a63879f83
Jan Kotvaš <jan.kotvas96@gmail.com>
2025-06-25 13:14:40 +0200

    feat: added basic router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Lobby</div>,
+  },
+  {
+    path: "/game",
+    element: <div>Game</div>,
+  },
+  {
+    path: "/endgame-stats",
+    element: <div>End Game Stats</div>,
+  },
+  {
+    path: "*",
+    element: <div>404 Not Found</div>,
+  },
+]);
+
 function App() {
   return (
     <>
-      <div className="flex flex-col gap-3 justify-center items-center h-screen bg-gray-100">
-        <h1 className="text-3xl">Welcome to My App</h1>
-        <p>This is a simple React application.</p>
-        <button
-          className="rounded-xl p-4 bg-blue-500 text-white hover:bg-blue-700 transition-colors duration-300 cursor-pointer"
-          onClick={() => alert("Button clicked!")}
-        >
-          Click Me
-        </button>
-      </div>
+      <RouterProvider router={router} />
     </>
   );
 }

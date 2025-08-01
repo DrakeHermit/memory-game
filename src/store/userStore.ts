bfbf5ace1c6bd455895ff169d8c2aedd3e3e30c5
Jan Kotvaš <jan.kotvas96@gmail.com>
2025-08-01 11:29:49 +0200

    feat: added a userStore for all the user related state needed

---
+++ src/store/userStore.ts
@@ -0,0 +1,24 @@
+import { create } from 'zustand';
+import type { User } from '../types/game';
+
+interface UserStore {
+  currentUser: User;
+  connectedPlayers: string[]
+  roomId: string;
+  connectionStatus: 'connected' | 'disconnected';
+  setCurrentUser: (user: User) => void;
+  setConnectedPlayers: (players: string[]) => void;
+}
+
+export const useUserStore = create<UserStore>((set) => ({
+  currentUser: {
+    id: 'player1',
+    score: 0,
+    isCurrentTurn: true,
+  },
+  connectedPlayers: [],
+  roomId: '',
+  connectionStatus: 'disconnected',
+  setCurrentUser: (user: User) => set({ currentUser: user }),
+  setConnectedPlayers: (players: string[]) => set({ connectedPlayers: players }),
+}));
--- src/types/game.ts
+++ src/types/game.ts
@@ -2,3 +2,9 @@ export interface Coin {
   id: number;
   value: number | string;
 }
+
+export interface User {
+  id: string
+  score: number
+  isCurrentTurn: boolean
+}

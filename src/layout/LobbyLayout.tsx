ba0d334ca7f705fee9e9de681e2495b2c63b5120
Jan Kotvaš <jan.kotvas96@gmail.com>
2025-12-31 11:54:41 +0100

    changed the padding on the header for mobile devices, it was too big

--- src/layout/LobbyLayout.tsx
+++ src/layout/LobbyLayout.tsx
@@ -2,8 +2,8 @@ import { Header } from "../components/Header";
 
 const LobbyLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-blue-950 min-h-screen text-center pt-[4.5rem] md:pt-[7rem] text-white">
-      <div className="md:mb-700 mb-600 text-center">
+    <div className="bg-blue-950 min-h-screen text-center pt-[2rem] md:pt-[7rem] text-white">
+      <div className="md:mb-700 mb-300 text-center">
         <Header />
       </div>
       <main className="text-white text-left max-w-[327px] mx-auto md:max-w-[654px]">
